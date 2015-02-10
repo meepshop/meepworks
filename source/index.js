c1a16c227d8a70eb20db86bbf1b7fdf9e11cf8bb
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-02-10 10:26:11 +0800

    clean up

--- source/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Views from './views/index';
-
-var Meepworks = {
-};
-
-export default Meepworks;
--- source/views/index.js
+++
@@ -1,6 +0,0 @@
-import HtmlPage from './html-page/html-page';
-
-var Views = {
-  HtmlPage: HtmlPage
-};
-export default Views;
