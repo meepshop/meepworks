5ee09c143e3940e476f76d2c10a1eb6d23d62d88
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-03-13 16:45:12 +0800

    remove debug logs

--- source/file-driver.js
+++ source/file-driver.js
@@ -9,22 +9,18 @@ import Tmpl from './tmpl';
 
 export default class FileDriver {
   constructor(App) {
-    console.log(App);
 
     //init stores
     let dispatcher = Dispatcher.getInstance();
     if(Array.isArray(App.stores)) {
       App.stores.forEach((s) => {
-        console.log('store: ', s);
         let t = s.getInstance();
-        console.log(t.handlers);
         t[INIT_STORE]();
         dispatcher.register(t);
       });
     }
 
     let AppComponent = App.component;
-    console.log(document.body);
     React.render(<AppComponent />, document.body);
 
   }
